1c1fa04b2bcecf0d57149e21f53f88b344d783bf
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-03 11:16:55 -0400

    Update stringManipulation.py

--- dayOne/stringManipulation.py
+++ dayOne/stringManipulation.py
@@ -1,6 +1,6 @@
 print("Hello World! \nHello World! \nHello World!")
 
-# the \n is an embedded symbol in the python language memory that indicates the creation of a new line while printing. This causes
+# the \n is an embedded symbol in the Python language memory that indicates the creation of a new line while printing. This causes
 # printing on a new line, even if there is only one print 'function'
 
 print("Hello" + "Izaan")
