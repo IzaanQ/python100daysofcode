1ed6b5a6d54746a961e5b6a659d5c98d568c5087
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-03 11:16:32 -0400

    Update stringManipulation.py

--- dayOne/stringManipulation.py
+++ dayOne/stringManipulation.py
@@ -4,3 +4,5 @@ print("Hello World! \nHello World! \nHello World!")
 # printing on a new line, even if there is only one print 'function'
 
 print("Hello" + "Izaan")
+
+# String concactenation is when you combine to strings in Python in order to output is as one.
