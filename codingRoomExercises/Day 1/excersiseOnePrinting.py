90aaca2f896f78fe7def1808cbf7ed4c03311da6
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-03 11:13:54 -0400

    Update excersiseOnePrinting.py

--- codingRoomExercises/Day 1/excersiseOnePrinting.py
+++ codingRoomExercises/Day 1/excersiseOnePrinting.py
@@ -1,12 +1,12 @@
-// desired Output: 
-// Day 1 - Python Print Function
-// The function is declared like this:
-// print('what to print')
+# desired Output: 
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
 
-// My Solution:
+# My Solution:
 
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
 
-// Result: 100% Match
+# Result: 100% Match
