477ac4cf5ae5d6856b540ca13e495b022dbf187e
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-03 11:12:56 -0400

    Update printingToTheConsole.py

--- dayOne/printingToTheConsole.py
+++ dayOne/printingToTheConsole.py
@@ -1,1 +1,4 @@
 print("Hello World!")
+
+# The 'print' funtion is an embedded funtion in the python language memory that is used to output whatever is enclosed within the parentheses
+# to the output console. It can output variables, but text must be enclosed in quotation marks in order to turn it into a string.
