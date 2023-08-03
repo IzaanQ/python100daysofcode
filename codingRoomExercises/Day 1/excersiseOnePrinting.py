481bfaf5d47b3aea08daa6595f42e2130792da3b
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-03 11:05:06 -0400

    Create excersiseOnePrinting.py

---
+++ codingRoomExercises/Day 1/excersiseOnePrinting.py
@@ -0,0 +1,11 @@
+// desired Output: Day 1 - Python Print Function
+//                         The function is declared like this:
+//                         print('what to print')
+
+// My Solution:
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+// Result: 100% Match
