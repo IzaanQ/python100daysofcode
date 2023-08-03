c221feb05ac2bf9b2811139b86ab00a1ae423c0d
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-03 17:13:50 -0400

    Create debuggingPractice.py

---
+++ codingRoomExercises/dayOne/debuggingPractice.py
@@ -0,0 +1,5 @@
+# Desired Output:
+# Day 1 - String Manipulation
+# String Concatenation is done with the "+" sign.
+# e.g. print("Hello " + "world")
+# New lines can be created with a backslash and n.
