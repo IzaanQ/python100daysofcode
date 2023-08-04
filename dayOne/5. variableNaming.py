75efb77c4d3d6de2e034dab2eccdedc4808a406d
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-04 08:57:25 -0400

    Create 5. variableNaming.py

---
+++ dayOne/5. variableNaming.py
@@ -0,0 +1,13 @@
+# There are naming rules that you should follow, to make your code readable
+# Make sure that your code makes sense for you
+
+username =/= user name
+username -> user_name
+
+# Use underscores to separate words in place of spaces
+
+# Reserved words should not be used for variable naming
+
+# e.g. input, print, len etc.
+
+# A NameError appears when you make a variable naming/calling mistake
