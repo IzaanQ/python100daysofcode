a4e9f7c49c4829411af05196114ff040d2645974
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-04 08:13:30 -0400

    Update pythonInputFunction.py

--- dayOne/pythonInputFunction.py
+++ dayOne/pythonInputFunction.py
@@ -1,1 +1,4 @@
 input("What is your name")
+
+# The \n is an embedded symbol in the Python language memory that indicates the creation of a new line while printing. This causes
+# printing on a new line, even if there is only one print 'function'
