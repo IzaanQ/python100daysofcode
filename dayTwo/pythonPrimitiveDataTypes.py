244e9ba44612e6b4899919961baf44aa6d59c03b
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-04 13:18:35 -0400

    Update pythonPrimitiveDataTypes.py

--- dayTwo/pythonPrimitiveDataTypes.py
+++ dayTwo/pythonPrimitiveDataTypes.py
@@ -1,1 +1,18 @@
 print(len("hello"))
+# >5
+
+print(len(1234))
+# Traceback (most recent call last):
+#  File "main.py", line 1, in <module>
+#    print(len(1234))
+# TypeError: object of type 'int' has no len()
+
+# Strings
+"hello", 'hello'
+# Series of characters encapsulated in double or single quotation marks
+
+# String Subscripting
+print("hello"[0])
+# >'h'
+# Remember that programmers start counting inclusive of zero
+
