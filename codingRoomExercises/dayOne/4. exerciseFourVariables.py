c2783add59ea30cc7267d2995259173e0a55ebb5
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-04 08:50:00 -0400

    Update 4. exerciseFourVariables.py

--- codingRoomExercises/dayOne/4. exerciseFourVariables.py
+++ codingRoomExercises/dayOne/4. exerciseFourVariables.py
@@ -1,2 +1,25 @@
 # Instructions:
 # Write a program that switches the values stored in the variables a and b.
+
+# Given:
+# 
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+# My Plan:
+# b = input(a: )
+# a = input(b: )
+
+# My Solution:
+
+b = input("a: ")
+a = input("b: ")
+
+# 🚨 Don't change the code below 👇
+print("a = " + a)
+print("b = " + b)
+# 🚨 Don't change the code above 👆
+
+# Result: 100% Match
