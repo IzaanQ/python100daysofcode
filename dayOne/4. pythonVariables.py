8d1d5d531d59d9caaad0275b2d8cf7d494d825e2
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-04 08:36:21 -0400

    Update 4. pythonVariables.py

--- dayOne/4. pythonVariables.py
+++ dayOne/4. pythonVariables.py
@@ -6,3 +6,5 @@ name = input("What is your name?")
 
 # This is called variable assignment. By doing this, we can assign the input function return value to the variable name. We can refer to this value
 # at any tiem as it is saved in the program.
+
+name = input("What is your name?")
