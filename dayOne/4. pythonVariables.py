ac430f2827fb6316c2830bb580d53ebd1f2ee157
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-04 08:35:23 -0400

    Update 4. pythonVariables.py

--- dayOne/4. pythonVariables.py
+++ dayOne/4. pythonVariables.py
@@ -1,1 +1,7 @@
 input("What is your name?")
+
+# Reminder: Whatever is passed by the user in the console as a result of this function is returned back to the function
+
+name = input("What is your name?")
+
+# This is called variable assignment. By doing this, we can assing the Input fun
