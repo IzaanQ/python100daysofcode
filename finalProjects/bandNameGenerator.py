cc0164ca4b7fb9a84c07794c6cd867469a676d24
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-04 09:06:04 -0400

    Update bandNameGenerator.py

--- finalProjects/bandNameGenerator.py
+++ finalProjects/bandNameGenerator.py
@@ -1,9 +1,8 @@
 print("Welcome to the Band Name Generator!")
 cityName = input("What city did you grow up in?\n")
 petName = input("What is your childhood pet's name?\n")
-
 bandName = cityName + " " + petName
 
-#5. Make sure the input cursor shows on a new line:
+print("your Band Name could be " + bandName)
 
 # Solution: https://replit.com/@appbrewery/band-name-generator-end
