10b8afd914355f7721d86223eaa234e1a6c0f586
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-04 08:18:43 -0400

    Update pythonInputFunction.py

--- dayOne/pythonInputFunction.py
+++ dayOne/pythonInputFunction.py
@@ -3,3 +3,5 @@ input("What is your name")
 # The Input Function is an embedded function in the Python language memory that displays to the console a prompt that is predetermined by the 
 # writer, which then is able to accept a user value via the console. Data that is entered by the user is passed back to the original input
 # function. The original Input Function is replaced with the value that the user send back via the console. 
+
+print("Hello " + input("What is your name"))
