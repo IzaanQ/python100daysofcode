4d9bc96f265f892c131a3ad59262aaaa8243e889
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-04 08:26:27 -0400

    Create exerciseThreeInputFunction.py

---
+++ codingRoomExercises/dayOne/exerciseThreeInputFunction.py
@@ -0,0 +1,14 @@
+# Instructions: 
+# Write a program that prints the number of characters in a user's name. Google can be used to determine the function that calculates the length
+# of a string.
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+
+# My Solution:
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# Result: 100% Match
