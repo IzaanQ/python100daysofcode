a173f9476234006030db73e94b0d2f551cf25673
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-06 18:18:08 -0400

    Update typeErrorCheckingAndConversion.py

--- dayTwo/typeErrorCheckingAndConversion.py
+++ dayTwo/typeErrorCheckingAndConversion.py
@@ -1,6 +1,7 @@
 # A function expects a certain kind of input. That input has a pre-determines type that is expected by the function.
 # When the input object doesnt meet the required type, you will get an error that indicates that you have the incorrect type for the input.
 
+# Type Checking
 type(variable name) # When printed, shows the Primitive Data Type of the variable
 
 # Type Changing/Casting
