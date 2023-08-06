232630452d8fa1089a543da4687885375bce09bb
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-06 18:01:09 -0400

    Create typeErrorCheckingAndConversion.py

---
+++ dayTwo/typeErrorCheckingAndConversion.py
@@ -0,0 +1,2 @@
+# A function expects a certain kind of input. That input has a pre-determines type that is expected by the function.
+# When the input object doesnt meet the required type, you will get an error that indicates that you have the incorrect type for the input.
