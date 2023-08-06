6dce5adc2f533317d7d8d12051b087dbf614ac36
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-06 18:14:11 -0400

    Update typeErrorCheckingAndConversion.py

--- dayTwo/typeErrorCheckingAndConversion.py
+++ dayTwo/typeErrorCheckingAndConversion.py
@@ -1,5 +1,5 @@
 # A function expects a certain kind of input. That input has a pre-determines type that is expected by the function.
 # When the input object doesnt meet the required type, you will get an error that indicates that you have the incorrect type for the input.
 
-type(var)
+type(variable name)
 # You can use this function to check the type of the variable that is passed through
