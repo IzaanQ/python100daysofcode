76d1a5bf1a1f08396c4c16a7341ec99016babe0f
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-06 18:37:13 -0400

    Update mathematicalOperatorsInPython.py

--- dayTwo/mathematicalOperatorsInPython.py
+++ dayTwo/mathematicalOperatorsInPython.py
@@ -7,3 +7,5 @@
 6 / 2 # This divides both integers (this will always return a float number)
 
 3 ** 3 # This raises 3 to the power of 3
+
+# Remember to use BEDMAS (Brackets, Exponents, Multiplication/Division, Addition/Subtraction)
