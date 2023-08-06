a2de3643bc548105e7d84517726016154a7bd12f
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-06 18:35:10 -0400

    Update mathematicalOperatorsInPython.py

--- dayTwo/mathematicalOperatorsInPython.py
+++ dayTwo/mathematicalOperatorsInPython.py
@@ -4,4 +4,6 @@
 
 3 * 3 # This multiplies both integers
 
-6 / 2 # This divides boht integers (this will always return a float number)
+6 / 2 # This divides both integers (this will always return a float number)
+
+3 ** 3 # This raises 3 to the power of 3
