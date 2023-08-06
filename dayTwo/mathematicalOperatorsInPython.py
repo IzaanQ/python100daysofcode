82ca981d8c126977a761b43fe344ccf521497d6e
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2023-08-06 18:34:34 -0400

    Create mathematicalOperatorsInPython.py

---
+++ dayTwo/mathematicalOperatorsInPython.py
@@ -0,0 +1,7 @@
+3 + 5 # This adds both integers together
+
+1 - 4 # This subtracts both integers
+
+3 * 3 # This multiplies both integers
+
+6 / 2 # This divides boht integers (this will always return a float number)
